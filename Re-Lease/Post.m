8fbca4124f7943098f45527bf5c4db02093862cb
Alois <aloisbarreras@Aloiss-MacBook-Air.local>
2015-03-06 23:43:11 -0500

    commit before switch

---
+++ Re-Lease/Post.m
@@ -0,0 +1,13 @@
+//
+//  Post.m
+//  Re-Lease
+//
+//  Created by Alois Barreras on 3/6/15.
+//  Copyright (c) 2015 Josip Injic. All rights reserved.
+//
+
+#import "Post.h"
+
+@implementation Post
+
+@end
