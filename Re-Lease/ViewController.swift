6d9ed06696143d12ac1f35727f56cfb0c432ee11
croatianSensation <jinjic@gmail.com>
2015-03-06 19:41:31 -0500

    Initial Commit

---
+++ Re-Lease/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Re-Lease
+//
+//  Created by Josip Injic on 3/6/15.
+//  Copyright (c) 2015 Josip Injic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
